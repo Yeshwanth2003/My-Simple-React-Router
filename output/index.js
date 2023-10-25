ac9f86d57c18c40c5d6487b3548e30e9aea86acd
Yeshwanth2003 <yeshwanth.rmm.8@gmail.com>
2023-10-25 14:17:53 +0530

    beta

--- output/index.js
+++
@@ -1,8 +0,0 @@
-import BRouter from "./lib/wrapper/BRouter";
-import useMyNavigate from "./lib/do-routing/useMyNavigate";
-import MyLink from "./lib/do-routing/MyLink";
-import MyRoute from "./lib/route-handler/MyRoute";
-function mkFun() {
-  console.log("LoL......");
-}
-export { BRouter, MyLink, useMyNavigate, MyRoute, mkFun };
--- output/lib/routerContexts/routeContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-const RouteContext = /*#__PURE__*/createContext({});
-export default RouteContext;
